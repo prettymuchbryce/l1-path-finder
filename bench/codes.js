024f1bcf357c3f18914a62b0384436436ea3280a
Bryce Neal <brycedneal@gmail.com>
2016-05-16 19:49:13 -0700

    Fix benchmark. Add easystar branch

--- bench/codes.js
+++ bench/codes.js
@@ -5,18 +5,18 @@ var easystar = require('./libraries/easystar')
 var andrea = require('./libraries/astar-andrea')
 
 module.exports = {
-  'l1-path-finder': l1path,
-  'easystar': easystar /*,
-  'astar-andrea (BROKEN)': andrea,
-  'node-pathfinding': nodePathFinding,
-  'pathfinding.js: astar': pathfinding.astar,
-  'pathfinding.js: bestFirst (BROKEN)': pathfinding.bestFirst,
-  'pathfinding.js: bfs': pathfinding.bfs,
-  'pathfinding.js: dijktra': pathfinding.dijkstra,
-  'pathfinding.js: jps': pathfinding.jps,
-  'pathfinding.js: biastar (BROKEN)': pathfinding.biastar,
-  'pathfinding.js: bibestFirst (BROKEN)': pathfinding.bibestFirst,
-  'pathfinding.js: bibfs': pathfinding.bibfs,
-  'pathfinding.js: bidijkstra': pathfinding.bidijkstra
-  */
+  // 'l1-path-finder': l1path,
+  'easystar': easystar ,
+  // 'astar-andrea (BROKEN)': andrea,
+  // 'node-pathfinding': nodePathFinding,
+  'pathfinding.js: astar': pathfinding.astar
+  // 'pathfinding.js: bestFirst (BROKEN)': pathfinding.bestFirst,
+  // 'pathfinding.js: bfs': pathfinding.bfs,
+  // 'pathfinding.js: dijktra': pathfinding.dijkstra,
+  // 'pathfinding.js: jps': pathfinding.jps,
+  // 'pathfinding.js: biastar (BROKEN)': pathfinding.biastar,
+  // 'pathfinding.js: bibestFirst (BROKEN)': pathfinding.bibestFirst,
+  // 'pathfinding.js: bibfs': pathfinding.bibfs,
+  // 'pathfinding.js: bidijkstra': pathfinding.bidijkstra
+  
 }
